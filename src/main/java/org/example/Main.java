eda413e3313e746fe5dfba0e0b1b58b2cada8ceb
Marian <marian.hopota@gmail.com>
2023-03-09 20:52:25 +0200

    PTG-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.time.Instant;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello Titi Aur");
+        printPeriodicDateAndTime();
+    }
+
+    public static void printPeriodicDateAndTime() {
+
+        while (true) {
+            try {
+                System.out.println(Instant.now());
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+
+            }
+        }
     }
 }
